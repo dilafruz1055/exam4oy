587bab83263c28eb8ac4483730774fe37d73f3c7
Dilafruz Shukurova <dilafruzshukurova36@gmail.com>
2024-05-13 10:40:40 +0500

    dan

---
+++ index.js
@@ -0,0 +1,61 @@
+// Examen JavaScript Amaliy savollar
+
+// 1-savol Arifmetik operatorla
+// alert(5 % 2);
+
+// 2-savol Math metodlari
+// let taxminiyson = Math.random() * 10;
+
+// 3-savol Math metodlari
+// let son = Math.floor(12.510);
+// alert(son);
+
+// 4-savol function
+// let soz = "Mars it school";
+// function uzunligi(){
+//     alert(soz.length)
+// }
+
+// uzunligi();
+
+// 5-savol for loop
+
+// for(let i = 0; i < 10; i++){
+// console.log("Mars it school")
+// }
+
+// 6-savol Array methods  
+// let harflar = ["a", "b", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "x", "y", "z"]
+// console.log(harflar[3] + harflar[8] + harflar[11] + harflar[1] + harflar[5] + harflar[17] + harflar[20] + harflar[24]);
+
+// 7-savol if else
+//  let yosh = Number(prompt("Yoshingizni kiriting:"));
+
+//  if (yosh >= 18) {
+    //  console.log("balogatka yetkan");
+//  } else if (yosh < 18) {
+    //  console.log("balogatka yetmagan");
+//  }
+
+// 8-savol String metodlari vs Array metodlari
+
+// let ism = prompt("Ismingizni kiriting:");
+//  let teskari = "";
+
+//  for (var i = ism.length - 1; i >= 0; i--) {
+//      teskari += ism[i];
+//  } 
+
+//  console.log(teskari);
+
+// 10-savol Array metodlari
+//  let sonlar = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+//  console.log("juftli sonlar:");
+
+//  for (let i = 0; i < sonlar.length; i ++) {
+//      if (sonlar[i] % 2 === 0) {
+//          console.log(sonlar[i]);
+//      }
+//  }
+ 
